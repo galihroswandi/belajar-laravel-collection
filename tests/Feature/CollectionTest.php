ecad21ad4c1551cf4252e7e7b50f85dc28e422b5
galihroswandi <galihroswandi25@gmail.com>
2023-09-06 11:05:46 +0700

    feat: Foreach

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -11,4 +11,11 @@ class CollectionTest extends TestCase
         $collection = collect([1, 2, 3, 4]);
         $this->assertEqualsCanonicalizing([1, 2, 3, 4], $collection->all());
     }
+
+    public function testForeach()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        foreach ($collection as $key => $value)
+            $this->assertEquals($key + 1, $value);
+    }
 }
